4769cc97e791a887e27b0deb8fb16ef2c148ad61
amira <amira7assan4444@gmail.com>
2025-09-24 14:23:25 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class LibraryStore extends StatelessWidget {
+  const LibraryStore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
